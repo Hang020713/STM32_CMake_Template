48995c39492ebf83a8ca4b1a18395c6f2fb3c6ea
B <hang020713@gmail.com>
2026-04-25 01:23:06 +0800

    feat: added files

---
+++ cmake/stm32-toolchain.cmake
@@ -0,0 +1,21 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+
+# Specify the cross compiler
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+set(CMAKE_AR arm-none-eabi-ar)
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP arm-none-eabi-objdump)
+set(CMAKE_SIZE arm-none-eabi-size)
+
+# Processor specific flags
+set(CPU_FLAGS "-mcpu=cortex-m3 -mthumb -mfloat-abi=soft")
+
+set(CMAKE_C_FLAGS_INIT "${CPU_FLAGS}")
+set(CMAKE_CXX_FLAGS_INIT "${CPU_FLAGS}")
+set(CMAKE_ASM_FLAGS_INIT "${CPU_FLAGS}")
+
+# Don't try to run the linker during compiler test
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
---
+++ src/stm32f1xx_hal_conf.h
@@ -0,0 +1,55 @@
+#ifndef __STM32F1xx_HAL_CONF_H
+#define __STM32F1xx_HAL_CONF_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/* Module Selection */
+#define HAL_MODULE_ENABLED
+#define HAL_CORTEX_MODULE_ENABLED
+#define HAL_DMA_MODULE_ENABLED
+#define HAL_FLASH_MODULE_ENABLED
+#define HAL_GPIO_MODULE_ENABLED
+#define HAL_PWR_MODULE_ENABLED
+#define HAL_RCC_MODULE_ENABLED
+#define HAL_UART_MODULE_ENABLED
+// Add other modules as needed
+
+/* Oscillator Values */
+#define HSE_VALUE    8000000U
+#define HSE_STARTUP_TIMEOUT    100U
+#define HSI_VALUE    8000000U
+#define LSI_VALUE    40000U
+#define LSE_VALUE    32768U
+#define LSE_STARTUP_TIMEOUT    5000U
+
+/* System Configuration */
+#define VDD_VALUE    3300U
+#define TICK_INT_PRIORITY    0x0FU
+#define USE_RTOS    0U
+#define PREFETCH_ENABLE    1U
+
+/* Assert macro */
+#ifdef  USE_FULL_ASSERT
+#define assert_param(expr) ((expr) ? (void)0U : assert_failed((uint8_t *)__FILE__, __LINE__))
+void assert_failed(uint8_t* file, uint32_t line);
+#else
+#define assert_param(expr) ((void)0U)
+#endif
+
+/* HAL includes */
+#include "stm32f1xx_hal_rcc.h"
+#include "stm32f1xx_hal_gpio.h"
+#include "stm32f1xx_hal_dma.h"
+#include "stm32f1xx_hal_cortex.h"
+#include "stm32f1xx_hal_flash.h"
+#include "stm32f1xx_hal_pwr.h"
+#include "stm32f1xx_hal_uart.h"
+// Include other HAL modules you enabled
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F1xx_HAL_CONF_H */
